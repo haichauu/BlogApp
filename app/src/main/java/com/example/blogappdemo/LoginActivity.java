8d11239a41300f2e834a0f8ef9df143dcb221403
HC <haichau@HCs-MacBook-Air.net.fpt>
2019-04-03 01:06:57 +0700

    Finish Register and Login Features

--- app/src/main/java/com/example/blogappdemo/LoginActivity.java
+++ app/src/main/java/com/example/blogappdemo/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.blogappdemo;
 
 import android.content.Intent;
+
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -32,11 +33,19 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-       // loginProgress = findViewById(R.id.login_progress);
+        loginProgress = findViewById(R.id.login_progress);
         btnLogin = findViewById(R.id.login_btn);
         btnRegister = findViewById(R.id.reg_btn);
         edtLoginEmail = findViewById(R.id.login_email);
         edtLoginPass = findViewById(R.id.login_password);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regintent = new Intent(LoginActivity.this, Register_Activity.class);
+                startActivity(regintent);
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +57,7 @@ public class LoginActivity extends AppCompatActivity {
                 if(!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(passwordEmail))
                 {
 
-                  // loginProgress.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.VISIBLE);
                     mAuth.signInWithEmailAndPassword(loginEmail, passwordEmail).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
@@ -65,12 +74,12 @@ public class LoginActivity extends AppCompatActivity {
                             }
 
 
-                            //loginProgress.setVisibility(View.INVISIBLE);
+                            loginProgress.setVisibility(View.INVISIBLE);
                         }
                     });
                 }
                 else
-                    Toast.makeText(LoginActivity.this, "Vui long nhap day du!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Please fill all information!", Toast.LENGTH_LONG).show();
 
             }
         });
--- app/src/main/java/com/example/blogappdemo/MainActivity.java
+++ app/src/main/java/com/example/blogappdemo/MainActivity.java
@@ -3,16 +3,31 @@ package com.example.blogappdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mainToolbar;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_main);
+
+        mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
+        getSupportActionBar().setTitle("Photo Blog");
 
 
 
@@ -26,11 +41,45 @@ public class MainActivity extends AppCompatActivity {
         if(currentUser == null)
         {
 
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(loginIntent);
-            finish();
+            sendToLogin();
 
         }
 
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch ((item.getItemId()))
+        {
+            case R.id.action_logout_btn:
+                logOut();
+                return  true;
+
+                default:
+                    return  false;
+        }
+
+
+    }
+
+    private void logOut() {
+        mAuth.signOut();
+        sendToLogin();
+    }
+
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+
 }
